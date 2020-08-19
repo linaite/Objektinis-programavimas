9cac0ca2372affa1e1b1acba8490900047798443
linaite <lina.dirmaite@gmail.com>
2020-08-19 11:01:29 +0300

    Distance, fuel, price apskaiciavimas

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php
+$distance = rand(1000, 2000);
+$consumption = 7.5;
+$price_1 = 1.3;
+
+$fuel_total = $distance * $consumption/ 100;
+$price_trip = $fuel_total * $price_1;
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -6,33 +15,13 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <style>
-        .bg-1 {
-            background-color: red;
-        }
-
-        .bg-2 {
-            background-color: green;
-        }
-
-        .bg-3 {
-            background-color: blue;
-        }
-
-        .bg-4 {
-            background-color: purple;
-        }
-
-        .bg-5 {
-            background-color: orange;
-        }
-    </style>
 </head>
 <body>
-<button class="bg-<?php print (rand(1, 5)); ?>">BTN1</button>
-<button class="bg-<?php print (rand(1, 5)); ?>">BTN2</button>
-<button class="bg-<?php print (rand(1, 5)); ?>">BTN3</button>
-<button class="bg-<?php print (rand(1, 5)); ?>">BTN4</button>
-<button class="bg-<?php print (rand(1, 5)); ?>">BTN5</button>
+<h1>Kelionės skaičiuoklė</h1>
+<ul>
+    <li><?php print "$distance km."; ?></li>
+    <li><?php print round($fuel_total) . ' l/100 km.'; ?></li>
+    <li><?php print round($price_trip) . ' Eur/l'; ?></li>
+</ul>
 </body>
 </html>
