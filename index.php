8ad6a8e56d07f931115bde6dd10b7ec2ea2d33c2
linaite <lina.dirmaite@gmail.com>
2020-08-21 12:09:48 +0300

    Foreach-su images

--- index.php
+++ index.php
@@ -1,137 +1,93 @@
 <?php
+$game = [
+    'objects' => [
+        [
+            'x' => 50,
+            'y' => 500,
+            'class' => 'car',
+        ],
+        [
+            'x' => 100,
+            'y' => 100,
+            'class' => 'ball',
+        ],
+        [
+            'x' => 1020,
+            'y' => 200,
+            'class' => 'house',
+        ],
+        [
+            'x' => 700,
+            'y' => 500,
+            'class' => 'person',
+        ],
+    ]
+];
 
-//1. Persirašykite masyvą sudarytą iš skaičių
-define('INIT_ARRAY', [7, 2, 8, 4, 5, 7, 16, -5, -6, -7, 15.22, 1.66, -69.55, 1, 1, 5, 7, 5]);
+?>
 
-//2.  Padauginti esamo masyvo narius iš 2
-$array_double = [];
-
-for ($i = 0; $i < count(INIT_ARRAY); $i++) {
-    $array_double[] = INIT_ARRAY[$i] * 2;
-}
-var_dump($array_double);
-
-//3.Pakelti masyvo narius kvadratu
-$array_qud = [];
-
-for ($i = 0; $i < count(INIT_ARRAY); $i++) {
-    $array_qud[] = INIT_ARRAY[$i] ** 2;
-}
-var_dump($array_qud);
-
-//4.Padauginti masyvo narius iš jų index'o (vietos masyve)
-$array_index = [];
-
-for ($i = 0; $i < count(INIT_ARRAY); $i++) {
-    $array_index[] = INIT_ARRAY[$i] * $i;
-}
-var_dump($array_index);
-
-//5. Atrinkti tiktai teigimų elementų masyvą
-$positive = [];
-
-for ($i = 0; $i < count(INIT_ARRAY); $i++) {
-    if (INIT_ARRAY[$i] > 0) {
-        $positive[] = INIT_ARRAY[$i];
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<style>
+    body {
+        min-height: 100vh;
+        background-image: url("https://einasau.lt/wp-content/uploads/2019/04/vietnam-sapa-countryside.jpg");
+        background-repeat: no-repeat;
+        background-size: cover;
+        position: relative;
     }
-}
-var_dump($positive);
 
-//6. Atrinkti tiktai neigiamų elementų masyvą
-$negative = [];
-
-for ($i = 0; $i < count(INIT_ARRAY); $i++) {
-    if (INIT_ARRAY[$i] < 0) {
-        $negative[] = INIT_ARRAY[$i];
+    .car {
+        background: url("https://cdn2.buyacar.co.uk/sites/buyacar/files/styles/w860/public/alfa-romeo-giulia67-1_0.jpg?itok=cM6fGydG");
+        background-size: contain;
+        background-repeat: no-repeat;
+        width: 300px;
+        height: 100px;
     }
-}
-var_dump($negative);
 
-//7. Atrinkti tiktai lyginių skaičių masyvą
-$even = [];
-
-for ($i = 0; $i < count(INIT_ARRAY); $i++) {
-    if (gettype(INIT_ARRAY[$i]) === 'integer' && INIT_ARRAY[$i] % 2 === 0) {
-        $even[] = INIT_ARRAY[$i];
+    .ball {
+        background: url("https://www.bcf.com.au/on/demandware.static/-/Sites-srg-internal-master-catalog/default/dw20bfd25f/images/520209/BCF_520209_hi-res.jpg");
+        background-size: contain;
+        background-repeat: no-repeat;
+        width: 100px;
+        height: 100px;
     }
-}
-var_dump($even);
 
-//8. Atrinkti tiktai nelyginių skaičių masyvą
-
-$odd = [];
-
-for ($i = 0; $i < count(INIT_ARRAY); $i++) {
-    if (gettype(INIT_ARRAY[$i]) === 'integer' && INIT_ARRAY[$i] % 2 !== 0) {
-        $odd[] = INIT_ARRAY[$i];
+    .house {
+        background: url("https://lh3.googleusercontent.com/mk4C8GtfBo8UmmwrDtQb1essiQPh_A1cN8S5liJ3jCRy1RSqAMCYgP6VW-yH70XFDic");
+        background-size: contain;
+        background-repeat: no-repeat;
+        width: 400px;
+        height: 400px;
     }
-}
-var_dump($odd);
 
-//10. Visas neigiamas vertes masyve padaryti teigiamomis
-
-$make_positive = [];
-
-for ($i = 0; $i < count(INIT_ARRAY); $i++) {
-    if (INIT_ARRAY[$i] < 0) {
-        $make_positive[] = INIT_ARRAY[$i] * -1;
-    } else {
-        $make_positive[] = INIT_ARRAY[$i];
+    .person {
+        background: url("https://toppng.com/uploads/preview/happy-person-11545688398rslqmyfw4g.png");
+        background-size: contain;
+        background-repeat: no-repeat;
+        width: 200px;
+        height: 400px;
     }
-}
-var_dump($make_positive);
 
-//11. Pakelti visas masyvo reikšmes laipsniu 'index'
-$array_power_index = [];
+    .object {
 
-for ($i = 0; $i < count(INIT_ARRAY); $i++) {
-    $array_power_index[] = INIT_ARRAY[$i] ** $i;
-}
-var_dump($array_power_index);
-
-//12.Atrinkti tik natūralių skaičių masyvą
-$array_natural = [];
-
-for ($i = 0; $i < count(INIT_ARRAY); $i++) {
-    if (gettype(INIT_ARRAY[$i]) === 'integer' && INIT_ARRAY[$i] > 0) {
-        $array_natural[] = INIT_ARRAY[$i];
+        position: absolute
     }
-}
-var_dump($array_natural);
-
-//13.Suapvalinti visas masyvo reikšmes iki sveikų skaičių
-$array_integers = [];
-
-for ($i = 0; $i < count(INIT_ARRAY); $i++) {
-    if (gettype(INIT_ARRAY[$i]) !== 'integer') {
-        $array_integers[] = intval(round(INIT_ARRAY[$i]));
-    } else {
-        $array_integers[] = INIT_ARRAY[$i];
-    }
-}
-var_dump($array_integers);
-
-//14. Atrinkti kas antrą elementą
-$array_every2 = [];
-
-for ($i = 0; $i < count(INIT_ARRAY); $i+=2) {
-    $array_every2[] = INIT_ARRAY[$i];
-}
-var_dump($array_every2);
-
-//16. Atrinkti kas penktą elementą
-$array_every5 = [];
-
-for ($i = 0; $i < count(INIT_ARRAY); $i += 5) {
-    $array_every5[] = INIT_ARRAY[$i];
-}
-var_dump($array_every5);
-
-
-
-
-
-
+</style>
+<body>
+<?php foreach ($game['objects'] as $object): ?>
+    <div style="left:<?php print $object['x']; ?>px; top:<?php print $object['y']; ?>px;"
+         class="<?php print $object['class']; ?> object"></div>
+<?php endforeach; ?>
+</body>
+</html>
 
 
 
