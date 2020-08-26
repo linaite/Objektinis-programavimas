968c23ae965fb26b7c7590b2f3643b22e002b64b
linaite <lina.dirmaite@gmail.com>
2020-08-26 18:17:19 +0300

    Form - boobs final

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php
+var_dump($_POST);
+
+$size = intval($_POST['size'] ?? 50);
+$message = empty($_POST) ? 'Paspausk mygtuką' : $size;
+
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -7,8 +15,22 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
+<style>
+    .boobs {
+        background-image: url("https://cdn.pixabay.com/photo/2017/09/05/18/31/boobs-2718689_640.png");
+        background-size: contain;
+        background-repeat: no-repeat;
+        width: <?php print $size; ?>px;
+        height: <?php print $size; ?>px;
+        margin: 150px 0 0 150px;
+    }
+</style>
 <body>
-
-<h2>Test4</h2>
+<form method="post">
+    <input type="range" min="1" max="100" value="<?php print $size; ?>" name="size">
+    <button>Let's do it!</button>
+</form>
+<p><?= $message; ?></p>
+<div class="boobs"></div>
 </body>
 </html>
