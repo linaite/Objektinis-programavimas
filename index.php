b4dece988d43dbd357265a15e35d895f34db77fb
linaite <lina.dirmaite@gmail.com>
2020-08-26 13:38:27 +0300

    Test

--- index.php
+++ index.php
@@ -1,11 +1,3 @@
-<?php
-var_dump($_POST);
-
-$size = intval($_POST['size'] ?? 25);
-$message = empty($_POST) ? 'Paspausk mygtuką' : $size;
-
-?>
-
 <!doctype html>
 <html lang="en">
 <head>
@@ -15,22 +7,8 @@ $message = empty($_POST) ? 'Paspausk mygtuką' : $size;
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
-<style>
-    .boobs {
-        background-image: url("https://cdn.pixabay.com/photo/2017/09/05/18/31/boobs-2718689_640.png");
-        background-size: contain;
-        background-repeat: no-repeat;
-        width: <?php print $size; ?>px;
-        height: <?php print $size; ?>px;
-        margin: 150px 0 0 150px;
-    }
-</style>
 <body>
-<form method="post">
-    <input type="range" min="1" max="100" value="50" name="size">
-    <button>Let's do it!</button>
-</form>
-<p><?= $message; ?></p>
-<div class="boobs"></div>
+
+<h1>TEST</h1>
 </body>
 </html>
